787f122fb6e74a4a392c5ff78cbeda4e35dff11a
brennoumobi <brenno@umobi.com.br>
2020-04-11 23:48:11 -0300

    Project organization

--- Example/Pods/Target Support Files/ConstraintBuilder/ConstraintBuilder-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ConstraintBuilder : NSObject
-@end
-@implementation PodsDummy_ConstraintBuilder
-@end
--- Example/Pods/Target Support Files/Pods-ConstraintBuilder_Tests/Pods-ConstraintBuilder_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ConstraintBuilder_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_ConstraintBuilder_Tests
-@end
